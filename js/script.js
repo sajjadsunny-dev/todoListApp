de8b68ccfca344fecb0724061df198bbbce897ff
sajjadsunnyofficial <sajjadhossainsunny.official@gmail.com>
2023-08-07 19:13:50 +0600

    icon replaced with text

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ let todoApp = document.querySelector('#todoApp');
 let todoInput = todoApp.querySelector('.todoInput');
 let todoBtn = todoApp.querySelector('.todoBtn');
 let ul = todoApp.querySelector('ul');
-//console.log(inputValue)
 
 function inputField() {
   let inputValue = todoInput.value;
@@ -12,11 +11,13 @@ function inputField() {
     alert('Please fill the task');
     return;
   }
-  // console.log(todoInput.value);
-  // inputValue = '';
-
   const taskLiEl = document.createElement("li");
   taskLiEl.classList.add("task-container");
+
+  const taskContents = document.createElement("div");
+  taskContents.classList.add("task-contents");
+
+  taskLiEl.appendChild(taskContents);
 
 
   const taskInputEl = document.createElement("input");
@@ -25,41 +26,44 @@ function inputField() {
   taskInputEl.value = inputValue;
   taskInputEl.setAttribute('readonly', 'readonly');
 
-  taskLiEl.appendChild(taskInputEl);
+  taskContents.appendChild(taskInputEl);
 
   const taskBtnEL = document.createElement('div');
   taskBtnEL.classList.add('task-btns');
 
   const taskEditEl = document.createElement('button');
   taskEditEl.classList.add('edit');
-  taskEditEl.innerHTML = 'Edit';
 
   const taskDeleteEl = document.createElement('button');
   taskDeleteEl.classList.add('delete');
-  taskDeleteEl.innerHTML = 'Delete';
+  taskDeleteEl.innerHTML = '<i class="fa-solid fa-trash"></i>';
 
   taskBtnEL.appendChild(taskEditEl);
   taskBtnEL.appendChild(taskDeleteEl);
 
   taskLiEl.appendChild(taskBtnEL);
 
+  const taskIconEl = document.createElement('i');
+  taskIconEl.classList.add('fa-solid', 'fa-pen-to-square');
+
+  taskEditEl.appendChild(taskIconEl);
+
   ul.appendChild(taskLiEl);
 
-
-
   taskEditEl.addEventListener('click', () => {
-    if (taskEditEl.innerHTML.toLowerCase() == "edit") {
-      taskEditEl.innerText = "Save";
+    if (taskIconEl.classList.contains("fa-pen-to-square")) {
+      taskIconEl.classList.remove("fa-pen-to-square");
+      taskIconEl.classList.add("fa-floppy-disk");
       taskInputEl.removeAttribute("readonly");
       taskInputEl.focus();
     } else {
-      taskEditEl.innerText = "Edit";
+      taskIconEl.classList.remove("fa-floppy-disk");
+      taskIconEl.classList.add("fa-pen-to-square");
       taskInputEl.setAttribute("readonly", "readonly");
     }
   });
 
   taskDeleteEl.addEventListener('click', () => {
-    // inputValue = '';
     ul.removeChild(taskLiEl);
   })
   todoInput.value = '';
